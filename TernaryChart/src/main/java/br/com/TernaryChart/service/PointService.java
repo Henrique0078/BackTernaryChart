8bce2d096c03eb9a47cf1ebdf54d29b60d45950c
Henrique Fontenele <henriquefontenele078@gmail.com>
2023-05-27 18:10:13 -0300

    Alteração PUT

--- TernaryChart/src/main/java/br/com/TernaryChart/service/PointService.java
+++ TernaryChart/src/main/java/br/com/TernaryChart/service/PointService.java
@@ -9,6 +9,8 @@ import br.com.TernaryChart.repository.PointRepository;
 import java.util.List;
 import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -42,10 +44,16 @@ public class PointService{
         return newPoint;
     }
 
-    @PutMapping
-    public Point updatePoint(@RequestBody Point point) {
-        Point newPoint = pointRepository.save(point);
-        return newPoint;
+    @PutMapping("/update/{cd_ponto}")
+    public ResponseEntity<Point> updatePoint(@RequestBody Point newPoint, @PathVariable Integer cd_ponto)  {
+        Optional<Point> oldPoint = pointRepository.findById(cd_ponto);
+        if(oldPoint.isPresent()){
+            pointRepository.save(newPoint);
+            pointRepository.deleteById(cd_ponto);
+            return new ResponseEntity<Point>(newPoint, HttpStatus.OK);
+        }
+        else
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
     }
     
     @DeleteMapping("/{cd_ponto}")
@@ -56,7 +64,7 @@ public class PointService{
     }
     
     @GetMapping("/findbyid/{cd_ponto}")
-    public Optional<Point> findById(@PathVariable Integer cd_ponto){
+    public Optional<Point> findbyId(@PathVariable Integer cd_ponto){
         Optional<Point> Point = pointRepository.findById(cd_ponto);
         return Point;
     }
